e3c2fc58aae658712198a70c1305dd06cebc899b
GerardoCen <gerardoon890@gmail.com>
2024-10-21 09:31:36 -0600

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Programa que me presenta
+Véronica Martínez Anaya
+3A Programación Matutino
+*/
+
+echo "Mi nombre es Gerardo Ceniceros Maldonado";
+echo "<br>", "Estudio en el CBTIS 89";
+echo "<br>", "En tercer semestre de la carrera de programación A Matutino";
+echo "<br>", "Vivo en la ciudad de Durango, Dgo. ";
+
+?>
